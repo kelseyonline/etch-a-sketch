3de88b7a2e29d667165693d2aa9fd805616d3323
Kelsey Dabrowski <kdabrowski103@gmail.com>
2024-02-11 14:50:02 -0500

    Add functions to generate and remove cells

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-const gridContainer = document.querySelector('.grid-container');
+const GRIDSIZE = 500; 
+let squaresPerSide = 16;
+let resolution = squaresPerSide * squaresPerSide;
 
-const boxes = document.querySelectorAll('.box');
+const submitButton = document.querySelector('#submit-btn');
+const clearButton = document.querySelector('#clear-btn');
 
-boxes.forEach(box => {
-    box.addEventListener('mouseover', function(){
-        box.style.backgroundColor = 'blue';
-    });
+
+// What are the backticks?
+const sketchbox = document.querySelector('#sketchbox');
+sketchbox.style.width = sketchbox.style.height = `${GRIDSIZE}px`;
+
+// What is 'this'?
+function setPenColor() {
+    this.style.backgroundColor = 'violet';
+}
+
+function generateGrid() {
+    for (let i = 0; i < (resolution); i++) {
+        const cell = document.createElement('div');
+
+        cell.style.width = cell.style.height = `${(GRIDSIZE / squaresPerSide) - 2}px`;
+        cell.classList.add('cell');
+
+        sketchbox.appendChild(cell);
+
+        cell.addEventListener('mouseover', setPenColor);
+    }
+}
+
+function removeCells() {
+    while (sketchbox.firstChild) {
+        sketchbox.removeChild(sketchbox.firstChild);
+    }
+}
+
+generateGrid();
+
+submitButton.addEventListener('click', function() {
+    squaresPerSide = parseInt(document.querySelector('#squares-per-side').value);
+    resolution = squaresPerSide * squaresPerSide;
+    console.log('Button has been clicked');
+    removeCells();
+    generateGrid();
+})
+
+clearButton.addEventListener('click', function() {
+    removeCells();
+    generateGrid();
 });
-
-
-
-
-
-
-
-
-
-
-
